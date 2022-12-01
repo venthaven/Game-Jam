8cf0312f1011d688ab221cb31fe85822479a8284
venthaven <111927433+venthaven@users.noreply.github.com>
2022-12-01 12:49:14 -0500

    enemy pathfinding

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -5,11 +5,16 @@ onready var Corpse = load("res://enemy/Corpse.tscn")
 onready var Bullet = load("res://projectiles/EnemyBullet.tscn")
 var Bullet_Container = null
 
+
+
 var dead = false
 
+onready var ray = $RayCast2D
+export var speed = 25
+export var looking_speed = 25
+var line_of_sight = false
 var points = []
 const margin = 1.5
-export var looking_speed = 100
 
 
 func _ready():
@@ -25,16 +30,23 @@ func _physics_process(_delta):
 			self.rotation  = new_angle
 			
 			var velocity = Vector2.ZERO
-			points = get_node("/root/Game/Navigation2D").get_simple_path(get_global_position(), player.global_position, true)
-			if points.size() > 1:
-				var distance = points[1] - get_global_position()
-				var movedirection = distance.normalized()
-				if movedirection.length() > margin or points.size() > 2:
-						velocity = direction*looking_speed
-				else:
-					velocity = Vector2(0, 0)
-				move_and_slide(velocity, Vector2(0,0))
-			update()
+			player = get_node_or_null("/root/Game/Player_Container/Player")
+			if player != null:
+				ray.cast_to = ray.to_local(player.global_position)
+				var c = ray.get_collider()
+				line_of_sight = false
+				if c and c.name == "Player":
+					line_of_sight = true
+				points = get_node("/root/Game/Navigation2D").get_simple_path(get_global_position(), player.global_position, true)
+				if points.size() > 1:
+					var distance = points[1] - get_global_position()
+					var movedirection = distance.normalized()
+					if distance.length() > margin or points.size() > 2:
+							velocity = movedirection*speed
+					else:
+						velocity = Vector2(0, 0)
+					move_and_slide(velocity, Vector2(0,0))
+				update()
 
 func hit():
 	print("hit")
@@ -44,7 +56,7 @@ func hit():
 
 
 func _on_Timer_timeout():
-	if dead != true:
+	if dead != true: #and line_of_sight == true
 		Bullet_Container = get_node_or_null("/root/Game/Bullet_Container")
 		if Bullet_Container != null:
 			var bullet = Bullet.instance()
