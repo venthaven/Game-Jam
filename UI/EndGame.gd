684de32ede177fcdf3312ed53411b5a5486249dc
Lydia Higgins <111927433+venthaven@users.noreply.github.com>
2022-12-01 15:24:21 -0500

    endgame fix (#10)
    Co-authored-by: khosek <112003508+khosek@users.noreply.github.com>

--- UI/EndGame.gd
+++ UI/EndGame.gd
@@ -2,9 +2,14 @@ extends Control
 
 func _ready():
 	$FinalScore.text = "Your score was " + str(Global.score)
+	get_tree().paused = true
+	
 
 func _on_PlayAgain_pressed():
-	var _scene = get_tree().change_scene("res://Game.tscn")
+	get_tree().paused = false
+	Global.hunger = 100
+	Global.score = 0
+	var _scene = get_tree().change_scene("res://UI/Main Menu/menu.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
