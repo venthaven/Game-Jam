4299d968c40608e175849b0723d41d7ca959f06f
khosek <khosek@iu.edu>
2022-11-30 21:40:57 -0500

    Enemies drop a corpse upon death

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -1,5 +1,8 @@
 extends KinematicBody2D
 
+onready var Corpse = load("res://enemy/Corpse.tscn")
+var Enemy_Container = null
+
 var dead = false
 
 func _ready():
@@ -7,4 +10,9 @@ func _ready():
 
 func hit():
 	print("hit")
+	Enemy_Container = get_node_or_null("/root/Game/Bullet_Container")
+	if Enemy_Container != null:
+		var corpse = Corpse.instance()
+		corpse.global_position = self.position
+		Enemy_Container.add_child(corpse)
 	queue_free()
